892c43283517ad8d5d6ea2ada06fc3e1013564bb
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-05-23 12:38:17 -0500

    Replace starting code body with that of exercise 9-1.

--- 9-6_Ice_Cream_Stand.py
+++ 9-6_Ice_Cream_Stand.py
@@ -12,8 +12,6 @@ class IceCreamStand:
 		"""Initialize the flavors attribute."""
 		self.flavors = flavors
 
-
-
 class Restaurant:
     """Attempt to model a restaurant."""
 
@@ -21,7 +19,6 @@ class Restaurant:
         """Initilize restaurant name and cuisine type attributes."""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        self.number_served = 2
 
     def describe_restaurant(self):
         """Display the name of the restaurant and describes the type of
@@ -32,28 +29,8 @@ class Restaurant:
         """Display whether the restaurant is open or closed."""
         print(f"{self.restaurant_name.title()} is currently open!")
 
-    def read_number_served(self):
-        """Print a statement showing the number of people served so far."""
-        print(
-            f"This restaurant has served {self.number_served} customers so far.")
-
-    def update_number_served(self, number_served):
-        """Set the number of customers served to the given value."""
-        self.number_served = number_served
-
-    def increase_number_served(self, number_served):
-        """Add a set amount to the number served."""
-        self.number_served += number_served
-
 
 restaurant = Restaurant('big-C', 'turkish food')
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
-# restaurant.number_served = 49
-restaurant.read_number_served()
-# restaurant.update_number_served(4_011)
-# restaurant.read_number_served()
-restaurant.increase_number_served(20)
-
-restaurant.read_number_served()
