735cc7975245182e515c271dd81a1cf0c252ab13
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-06-01 13:19:59 -0500

    Create 'Admin' child class.

--- 9-7_Admin.py
+++ 9-7_Admin.py
@@ -1,10 +1,21 @@
-"""9-7. Admin: An administrator is a special kind of user. Write a class called 'Admin' that inherits from the 'User' class you wrote in Exercise 9-3. or Exercise 9-5. Add an attribute, privileges, that stores a list of strings like 'can add post', 'can delete post', 'can ban user', and so on. Write a method called 'show_privleges()' that lists the administrator's set of privileges. Create and instance of 'Admin', and call your method."""
+"""9-7. Admin: An administrator is a special kind of user. Write a class 
+called 'Admin' that inherits from the 'User' class you wrote in Exercise 9-3. 
+or Exercise 9-5. Add an attribute, privileges, that stores a list of strings 
+like 'can add post', 'can delete post', 'can ban user', and so on. Write a 
+method called 'show_privleges()' that lists the administrator's set of 
+privileges. Create and instance of 'Admin', and call your method."""
 
-"""9-3. Users: Make a class called User. Create two attributes called first_name and last_name, and then create several other attributes that are typically stored in a user profile. Make a method called describe_user() that prints a summary of the user's information. Make another method called greet_user() that prints a personalized greeting to the user. Create several instances representing different users, and call both methods for each user."""
+"""9-3. Users: Make a class called User. Create two attributes called 
+first_name and last_name, and then create several other attributes that are 
+typically stored in a user profile. Make a method called describe_user() 
+that prints a summary of the user's information. Make another method called 
+greet_user() that prints a personalized greeting to the user. Create several 
+instances representing different users, and call both methods for each 
+user."""
 
 
 class User:
-    """Attempt to model a user on a website."""
+    # Attempt to model a user on a website.
 
     def __init__(self, first_name, last_name, age, location, occupation):
         """Initialize first and last name attributes."""
@@ -22,6 +33,16 @@ class User:
         """Greet the given user with a personalized message."""
         print(
             f"Hello there {self.first_name.title()}!. I'm glad you are here with us today.\n")
+
+
+class Admin(User):
+    """Creates a type of User with extended privileges."""
+
+    def __init__(self, first_name, last_name, age, location, occupation):
+        """Initialize attributes of the parent class.
+        Then initialize attributes specific to the Admin user type."""
+        super().__init__(first_name, last_name, age, location, occupation)
+        self.privileges = ['can add post', 'can delete post', 'can ban user']
 
 
 user_1 = User('bilbo', 'baggins', 90, 'the shire', 'burglar')
