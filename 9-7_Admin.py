0214523bf7c8582bfd70d107a58ea5a7bba3661b
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-06-01 13:28:22 -0500

    Create 'show_privileges' class method.

--- 9-7_Admin.py
+++ 9-7_Admin.py
@@ -1,16 +1,16 @@
-"""9-7. Admin: An administrator is a special kind of user. Write a class 
-called 'Admin' that inherits from the 'User' class you wrote in Exercise 9-3. 
-or Exercise 9-5. Add an attribute, privileges, that stores a list of strings 
-like 'can add post', 'can delete post', 'can ban user', and so on. Write a 
-method called 'show_privleges()' that lists the administrator's set of 
+"""9-7. Admin: An administrator is a special kind of user. Write a class
+called 'Admin' that inherits from the 'User' class you wrote in Exercise 9-3.
+or Exercise 9-5. Add an attribute, privileges, that stores a list of strings
+like 'can add post', 'can delete post', 'can ban user', and so on. Write a
+method called 'show_privleges()' that lists the administrator's set of
 privileges. Create and instance of 'Admin', and call your method."""
 
-"""9-3. Users: Make a class called User. Create two attributes called 
-first_name and last_name, and then create several other attributes that are 
-typically stored in a user profile. Make a method called describe_user() 
-that prints a summary of the user's information. Make another method called 
-greet_user() that prints a personalized greeting to the user. Create several 
-instances representing different users, and call both methods for each 
+"""9-3. Users: Make a class called User. Create two attributes called
+first_name and last_name, and then create several other attributes that are
+typically stored in a user profile. Make a method called describe_user()
+that prints a summary of the user's information. Make another method called
+greet_user() that prints a personalized greeting to the user. Create several
+instances representing different users, and call both methods for each
 user."""
 
 
@@ -44,6 +44,12 @@ class Admin(User):
         super().__init__(first_name, last_name, age, location, occupation)
         self.privileges = ['can add post', 'can delete post', 'can ban user']
 
+    def show_privleges(self):
+        """Display the privileges held by an 'Admin' user type."""
+        print(
+            f"\n{self.first_name.title()} {self.last_name.title()} has the following privileges.")
+        print(self.privileges)
+
 
 user_1 = User('bilbo', 'baggins', 90, 'the shire', 'burglar')
 user_2 = User('gandalf', 'the gray', 190, 'middle earth', 'wizard')
@@ -57,3 +63,4 @@ user_2.describe_user()
 user_3.describe_user()
 # user_3.greet_user()
 user_4.describe_user()
+user_4.show_privleges()
