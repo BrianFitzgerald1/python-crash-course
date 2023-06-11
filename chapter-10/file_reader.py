c625c0c3c0515b4562ebfc17254f1886cc83a915
Brian <brianfitzgerald909@gmail.com>
2023-06-11 17:57:53 -0500

    Revise file path name to point to 'text_files' directory within 'chapter-10' directory.

--- chapter-10/file_reader.py
+++ chapter-10/file_reader.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
-path = Path('pi_digits.txt')
-contents = path.read_text()
+
+path = Path('text_files/pi_digits.txt')
+# path = Path('pi_digits.txt')
+contents = path.read_text().rstrip()
+# contents = contents.rstrip()
 print(contents)
