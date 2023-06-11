f74db38149eebd457e0090c949d252cec195163b
Brian <brianfitzgerald909@gmail.com>
2023-06-11 18:19:45 -0500

    Add 'lines' variable and for loop to 'file_reader.py'

--- chapter-10/file_reader.py
+++ chapter-10/file_reader.py
@@ -5,4 +5,8 @@ path = Path('text_files/pi_digits.txt')
 # path = Path('pi_digits.txt')
 contents = path.read_text().rstrip()
 # contents = contents.rstrip()
-print(contents)
+# print(contents)
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
