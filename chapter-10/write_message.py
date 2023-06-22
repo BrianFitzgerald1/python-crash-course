54c4d9c44b340fa9df65a8f9ada2776fe21b0b5f
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-06-22 13:13:07 -0500

    Add multi-line string to 'contents' variable.

--- chapter-10/write_message.py
+++ chapter-10/write_message.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 
+contents = "I love programming.\n"
+contents += "I love creating new games.\n"
+contents += "I also love working with data.\n"
+
+
 path = Path('programming.txt')
 path.write_text("I love programming.")
