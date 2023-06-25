d6478fac47b18dbc550af332b96feba1ad3e0ffc
Brian <brianfitzgerald909@gmail.com>
2023-06-25 15:34:30 -0500

    Add exercise instructions.

---
+++ chapter-10/10-5_Guest_Book.py
@@ -0,0 +1,4 @@
+"""10-5. Guest Book: Write a while loop that prompts users for their name.
+Collect all the names that are entered, and then write these names to a file
+called 'guest_book.txt'. Make sure each entry appears on a new line in the
+file."""
