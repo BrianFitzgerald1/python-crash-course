e00d955bcde0b9f37ec1b629a0667fe61c158752
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-06-29 12:43:49 -0500

    Comment out except code and replace with 'pass'.

--- chapter-10/word_count.py
+++ chapter-10/word_count.py
@@ -6,7 +6,8 @@ def count_words(path):
     try:
         contents = path.read_text(encoding='utf-8')
     except FileNotFoundError:
-        print(f"Sorry, the file {path} does not exist.")
+        # print(f"Sorry, the file {path} does not exist.")
+        pass
     else:
         # Count the approximate number of words in the file:
         words = contents.split()
