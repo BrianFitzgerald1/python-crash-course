08c71073e3f8a7c6d75e4e49a60b556921e55f50
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-06-30 12:47:21 -0500

    Print a string containing user's input on screen to verify input to user.

--- chapter-10/10-6_Addition.py
+++ chapter-10/10-6_Addition.py
@@ -4,13 +4,14 @@ prompt = "Give me any two numbers and I will add them together for you.\n"
 print(prompt)
 
 input_1 = input("Enter the first number: ")
-input_2 = input("Please enter the second number: ")
+print(f"The value you entered is {input_1}.\n")
 
+input_2 = input("Please enter the second number: ")
+print(f"The value you entered is {input_1}.\n")
 
 number_1 = int(input_1)
-print(f"The value you entered is {input_1}.")
 
 number_2 = int(input_2)
 
 result = number_1 + number_2
-print(result)
+print(f"Adding the two numbers produces {result}.")
