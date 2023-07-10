df0da8c19f196010c84ba1300529df24e4bbbc0e
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-10 13:30:32 -0500

    Create try/except block and add function docstring.

--- chapter-10/10_7_test.py
+++ chapter-10/10_7_test.py
@@ -1,2 +1,10 @@
-def get_input_1:
-    
+def get_input_1(input_1):
+    """Get a number from the user and verify it is an integer."""
+    input_1 = input("Enter an integer: ")
+    try:
+        number_1 = int(input_1)
+    except ValueError:
+        print(f"{input_1} is not an integer.")
+
+
+get_input_1()
