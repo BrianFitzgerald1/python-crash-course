d9d2ce0b32d08b4bffa130a2f9baafe8caa72e33
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-10 13:42:47 -0500

    Add excercise 10-7 instructions docstring.

--- chapter-10/10-7_Addition_Calculator.py
+++ chapter-10/10-7_Addition_Calculator.py
@@ -1,8 +1,10 @@
+"""10-7. Addition Calculator: Wrap your code from Exercise 10-5 in a while loop so the user can continue entering numbers, even if they make a mistake and enter text instead of a number."""
+
 """10-5. Guest Book: Write a while loop that prompts users for their name. Collect all the names that are entered, and then write these names to a file called 'guest_book.txt'. Make sure each entry appears on a new line in the
 file."""
 
-from pathlib import Path
 
+from pathlib import Path
 path = Path('chapter-10/text_files/guest_book.txt')
 
 names = ''
