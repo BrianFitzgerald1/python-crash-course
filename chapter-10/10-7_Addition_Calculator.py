8ca4317e8c884f1662aab5083ad4b630b341aa80
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-13 12:40:57 -0500

    Create while loop for continuous input of numbers by the user.

--- chapter-10/10-7_Addition_Calculator.py
+++ chapter-10/10-7_Addition_Calculator.py
@@ -2,6 +2,7 @@
 
 """10-6. Addition: One common problem when programming for numerical input occurs when people provide text instead of numbers. When you try to convert the input to an int, you'll get a ValueError. Write a program that prompts for two numbers. Add them together and print the result. Catch the ValueError if either input value is not a number, and print a friendly error message. Test your program by entering two numbers and then by entering some text instead of a number."""
 
+while number
 try:
     number_1 = input("Enter a number: ")
     number_1 = int(number_1)
