5015280b643847f3894f88caf6116d1d72bfd79e
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-18 12:24:30 -0500

    Revise 'path' and 'contents' variable names to accomodate referencing a second text file. Add addditional try/except block to check for second text file.

--- chapter-10/10-8_Cats_and_Dogs.py
+++ chapter-10/10-8_Cats_and_Dogs.py
@@ -1,12 +1,20 @@
-"""10-8. Cats and Dogs: Make two files, cats.txt and dogs.txt. Store at least three names of cats in the first file and three names of dogs in the second file. Write a program that tries to read these files and print the contents of the file to the screen. Wrap your code in a try-except block to catch the FileNotFound error, and print a friendly message if a file is missing. Move one of the files to a dif- ferent location on your system, and make sure the code in the except block executes properly."""
+"""10-8. Cats and Dogs: Make two files, cats.txt and dogs.txt. Store at least three names of cats in the first file and three names of dogs in the second file. Write a program that tries to read these files and print the contents of the file to the screen. Wrap your code in a try-except block to catch the FileNotFound error, and print a friendly message if a file is missing. Move one of the files to a different location on your system, and make sure the code in the except block executes properly."""
 
 from pathlib import Path
 
-path = Path('text_files/cats.txt')
+path_1 = Path('text_files/cats.txt')
+path_2 = Path('text_files/dogs.txt')
 
 try:
-    contents = path.read_text()
-    print(contents)
+    contents_1 = path_1.read_text()
+    print(contents_1)
+
+except FileNotFoundError:
+    print("Sorry, but this file can't be found in this directory.\nTry switching to another directory.")
+
+try:
+    contents_2 = path_2.read_text()
+    print(contents_2)
 
 except FileNotFoundError:
     print("Sorry, but this file can't be found in this directory.\nTry switching to another directory.")
