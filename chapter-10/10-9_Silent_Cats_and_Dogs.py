3cce3bfa06166045c69c275de9a94c3e73201506
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-18 12:28:07 -0500

    Paste in starting code body.

--- chapter-10/10-9_Silent_Cats_and_Dogs.py
+++ chapter-10/10-9_Silent_Cats_and_Dogs.py
@@ -1,1 +1,20 @@
 """Modify your except block in Exercise 10-8 to fail silently if either file is missing."""
+
+from pathlib import Path
+
+path_1 = Path('text_files/cats.txt')
+path_2 = Path('text_files/dogs.txt')
+
+try:
+    contents_1 = path_1.read_text()
+    print(contents_1)
+
+except FileNotFoundError:
+    print("Sorry, but this file can't be found in this directory.\nTry switching to another directory.")
+
+try:
+    contents_2 = path_2.read_text()
+    print(contents_2)
+
+except FileNotFoundError:
+    print("Sorry, but this file can't be found in this directory.\nTry switching to another directory.")
