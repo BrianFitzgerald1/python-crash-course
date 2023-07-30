8f98c217704f0d442daee9ce4e9baa6f661ff3da
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-30 14:15:42 -0500

    Move code body into 'greet_user()' function definition.

--- chapter-10/remember_me.py
+++ chapter-10/remember_me.py
@@ -5,15 +5,13 @@ import json
 def greet_user():
     """Greet the user by name."""
 
-
-path = Path('username.json')
-
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("What is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"We'll remember you when you come back, {username}!")
+    path = Path('username.json')
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"We'll remember you when you come back, {username}!")
