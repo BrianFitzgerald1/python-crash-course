dcca83802350b62e15dfed56e005f13dd877f9ef
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-07-30 14:42:44 -0500

    Create 'get_new_username()' function and refactor 'greet_user()'.

--- chapter-10/remember_me.py
+++ chapter-10/remember_me.py
@@ -12,6 +12,14 @@ def get_stored_username(path):
         return None
 
 
+def get_new_username(path):
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
+
+
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
@@ -19,9 +27,7 @@ def greet_user():
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        contents = json.dumps(username)
-        path.write_text(contents)
+        username = get_new_username(path)
         print(f"We'll remember you when you come back, {username}!")
 
 
