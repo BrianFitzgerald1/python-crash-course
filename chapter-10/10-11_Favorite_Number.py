36fae46cf318a4d9e497d1fcab429b01b0c325eb
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-08-01 13:01:05 -0500

    Add exercise docstring. Request user input for their favorite number and store input in 'fav_number' variable. Print back the user input to user.

--- chapter-10/10-11_Favorite_Number.py
+++ chapter-10/10-11_Favorite_Number.py
@@ -1,34 +1,8 @@
+"""10-11. Favorite Number: Write a program that prompts for the user's favorite number. Use json.dumps() to store this number in a file. Write a separate program that reads in this value and prints the message 'I know your favorite number! It's ___!'"""
+
 from pathlib import Path
 import json
 
-
-def get_stored_username(path):
-    """Get stored username if available."""
-    if path.exists():
-        contents = path.read_text()
-        username = json.loads(contents)
-        return username
-    else:
-        return None
-
-
-def get_new_username(path):
-    """Prompt for a new username."""
-    username = input("What is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
-    return username
-
-
-def greet_user():
-    """Greet the user by name."""
-    path = Path('username.json')
-    username = get_stored_username(path)
-    if username:
-        print(f"Welcome back, {username}!")
-    else:
-        username = get_new_username(path)
-        print(f"We'll remember you when you come back, {username}!")
-
-
-greet_user()
+fav_number = input("Tell me your favorite number and I'll remember it. ")
+print(
+    f"Your number is {fav_number}. I'll remember that the next time we meet.")
