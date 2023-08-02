ea344ffa84b1825defe9bbaeca9458766c086191
Brian Fitzgerald <BrianFitzgerald909@gmail.com>
2023-08-02 15:52:42 -0500

    Import Path and json. Create reference to json file to be created.

--- chapter-10/10-13_User_Dictionary.py
+++ chapter-10/10-13_User_Dictionary.py
@@ -1,1 +1,6 @@
 """10-13_User_Dictionary: The 'remember_me.py' example only stores one piece of information, the username. Expand this example by asking for two more pieces of information about the user, then store all the information you collect ina dictonary. Write this dictionary to a file using json.dumps(), and read it back in using json.loads(). Print a summary showing exactly what your program remembers about the user."""
+
+from pathlib import Path
+import json
+
+path = Path('user_dict.json')
