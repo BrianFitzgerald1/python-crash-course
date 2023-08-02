baa5806a06ff7c926d158a8d44e2141a813242d1
Brian <brianfitzgerald909@gmail.com>
2023-08-02 18:20:53 -0500

    Rearange codeblock for better readability.

--- chapter-10/remember_me.py
+++ chapter-10/remember_me.py
@@ -2,6 +2,17 @@ from pathlib import Path
 import json
 
 
+def greet_user():
+    """Greet the user by name."""
+    path = Path('username.json')
+    username = get_stored_username(path)
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username(path)
+        print(f"We'll remember you when you come back, {username}!")
+
+
 def get_stored_username(path):
     """Get stored username if available."""
     if path.exists():
@@ -18,17 +29,6 @@ def get_new_username(path):
     contents = json.dumps(username)
     path.write_text(contents)
     return username
-
-
-def greet_user():
-    """Greet the user by name."""
-    path = Path('username.json')
-    username = get_stored_username(path)
-    if username:
-        print(f"Welcome back, {username}!")
-    else:
-        username = get_new_username(path)
-        print(f"We'll remember you when you come back, {username}!")
 
 
 greet_user()
